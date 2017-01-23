59f47c7c63f3a5dcc0450cda86e70f48679dc563
Benjamin Allen <benjaminallen1985@yahoo.com>
2017-01-22 21:00:34 -0600

    First Commit of BlockBreaker scripts

---
+++ Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;		
+	}
+	
+	void Update () {
+		if (!hasStarted) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;		
+			if (Input.GetMouseButtonDown(0)) {			
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);			
+			}
+		}	
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {	
+		Vector2 tweak = new Vector2 (Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
+		if(hasStarted) {		
+			audio.Play();
+			rigidbody2D.velocity += tweak;
+		}
+	}
+}
---
+++ Brick.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	public GameObject smoke;
+	
+	private bool isBreakable;
+	private int timesHit;	
+	private LevelManager levelManager;
+	
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			breakableCount++;		
+	}
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+	void Update () {
+	}
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} else {
+			Debug.LogError ("Brick sprite missing");
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.5f);
+		if(isBreakable) {
+			HandleHits();
+		}
+	}
+	
+	void HandleHits () {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			breakableCount--;
+			PuffSmoke();
+			levelManager.BrickDestroyed();
+			Destroy(gameObject);
+		} else {
+			LoadSprites();	
+		}
+	}
+	
+	void PuffSmoke() {
+		GameObject smokePuff = Instantiate (smoke, transform.position, Quaternion.identity) as GameObject;
+		smokePuff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+	
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
